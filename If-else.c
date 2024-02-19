246d21b627b5df37a05584f3eb6f2d645ba72b16
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-02-18 20:30:18 -0800

    [ADDED]:shubham  if-else Condition to check No Even or Odd

---
+++ If-else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void  main()
+{
+	int n;
+	printf("Enter the any no=\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("\nNo is Even");
+	}
+	else
+	{
+		printf("\nNo is odd");
+	}
+	return 0;
+}
