2108f34549cba9ca17e14eba9a74c10b6a4aa651
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-18 22:27:41 +0530

    [ADDED]:shubham This code for Multiplication Demo

---
+++ MultiplicationDemo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c=0;
+	printf("Enter the value of A=");
+	scanf("%d",&a);
+	printf("Enter the value of B=");
+	scanf("%d",&b);
+	c=a*b;
+	printf("multiplication = %d",c);
+	return 0;
+}
