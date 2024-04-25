bb937473914c2cd0f3c2ccd941f8266af5b023fa
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-25 21:58:03 +0530

    [ADDED]:shubham This code for Arithmatic Opretion Demo

---
+++ Arithmatic_opreDemo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("Enter The value of  A=");
+	scanf("%d",&a);
+	printf("Enter The value of  B=");
+	scanf("%d",&b);
+
+	c=a+b;
+	printf("\nAddition=%d",c);
+	return 0;
+}
